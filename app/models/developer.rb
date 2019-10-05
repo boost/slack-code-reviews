322bc679669afedf56a9242bf956ab3d7a952f59
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-10-06 01:00:32 +1300

    the requester should not be picked up as a reviewer

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,4 +2,20 @@
 
 class Developer < ApplicationRecord
   belongs_to :team
+
+  scope :in_team, ->(team) { where(team: team) }
+
+  def self.pick_for_review(team, name = nil, do_not_pick = [])
+    if name
+      reviewer = find_by(team: team, name: name)
+      raise ActiveRecord::RecordNotFound, "<#{name}> not found" unless reviewer
+    else
+      query = in_team(team)
+      do_not_pick.each { |dev| query = query.where.not(id: dev.id) }
+      reviewer = query.sample
+      raise ActiveRecord::RecordNotFound, 'Could not find an available reviewer' unless reviewer
+    end
+
+    reviewer
+  end
 end
--- app/services/slack/action/create_code_review.rb
+++ app/services/slack/action/create_code_review.rb
@@ -6,37 +6,18 @@ module Slack
       def initialize(args)
         super(args)
         @visibility = :in_channel
+        # to be able using the cli waiting for proper cli paramters
+        args.list = args.list[0].split('|') if args.list[0].include?('|')
 
-        puts "URL: #{args.list[0]}"
-        puts "REVIEWER1: #{args.list[1]}"
-        puts "REVIEWER2: #{args.list[2]}"
-        url = args.list[0]
-        reviewer1 = find_or_pick_a_reviewer(args.list[1])
-        reviewer2 = find_or_pick_a_reviewer(args.list[2], reviewer1)
+        dont_pick = args.requester ? [args.requester] : []
+        reviewer1 = Developer.pick_for_review(args.team, args.list[1], dont_pick)
+        reviewer2 = Developer.pick_for_review(args.team, args.list[2], dont_pick + [reviewer1])
 
-        @text = "New CR: #{url} <#{reviewer1.name}> <#{reviewer2.name}>"
-      rescue NotEnoughDeveloperError => e
+        @text = "New CR: #{args.list[0]} <#{reviewer1.name}> <#{reviewer2.name}>"
+      rescue ActiveRecord::RecordNotFound => e
         @visibility = :ephemeral
         @text = e.message
       end
-
-      private
-
-        def find_or_pick_a_reviewer(name, do_not_pick = nil)
-          reviewer = Developer.find_by(team: @team, name: name)
-          return reviewer if reviewer
-
-          request = Developer.where(team: @team)
-          request = request.where.not(id: do_not_pick.id) unless do_not_pick.nil?
-
-          if request.count < 2
-            raise NotEnoughDeveloperError,
-                  'You must have at least 2 developers to create a code review.'
-          end
-
-          # to improve to use a queue instead of a random system
-          request.offset(rand(request.count)).first
-        end
     end
   end
 end

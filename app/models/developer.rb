b2207364bd3cafc0045c627bb112ab7844f07ed1
Montgomery Anderson <montgomery@boost.co.nz>
2019-10-18 11:40:36 +1300

    Implemented sampling the next reviewers off of a rotating queue based of the last code review they were assigned to

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -3,19 +3,40 @@
 class Developer < ApplicationRecord
   belongs_to :slack_workspace
 
-  scope :in_slack_workspace, ->(slack_workspace) { where(slack_workspace: slack_workspace) }
+  has_many :reviewers
+  has_many :code_reviews, through: :reviewers
 
-  def self.pick_for_review(slack_workspace, name = nil, do_not_pick = [])
-    if name
-      reviewer = find_by(slack_workspace: slack_workspace, name: name)
-      raise ActiveRecord::RecordNotFound, "<#{name}> not found" unless reviewer
-    else
-      query = in_slack_workspace(slack_workspace)
-      do_not_pick.each { |dev| query = query.where.not(id: dev.id) }
-      reviewer = query.sample
-      raise ActiveRecord::RecordNotFound, 'Could not find an available reviewer' unless reviewer
+  scope :in_slack_workspace,      ->(workspace) { where(slack_workspace: workspace) }
+  scope :havnt_recieved_a_review, -> { where(code_reviews: []) }
+
+  class << self
+    def pick_reviewer(workspace, name: nil, exclude: [])
+      return pick_specific_reviewer(workspace, name) if name
+
+      pick_next_reviewer(workspace, exclude: exclude)
     end
 
-    reviewer
+    # Pick a specific develoepr for code review
+    def pick_specific_reviewer(workspace, name)
+      reviewer = find_by(slack_workspace: workspace, name: name)
+      return reviewer if reviewer.present?
+
+      raise ActiveRecord::RecordNotFound, "<#{name}> not found"
+    end
+
+    # Pick the next developer up for code review
+    def pick_next_reviewer(workspace, exclude: [])
+      workspace_developers = in_slack_workspace(workspace)
+
+      available_developers = workspace_developers.to_a - exclude
+
+      reviewer = available_developers.min_by do |developer|
+        developer.code_reviews.last&.created_at || DateTime.new(0) # along time ago
+      end
+
+      return reviewer if reviewer.present?
+
+      raise ActiveRecord::RecordNotFound, 'Could not find an available reviewer'
+    end
   end
 end

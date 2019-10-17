71325a77d636a1e540afd27b7ca5a5f6d5ce8436
Montgomery Anderson <montgomery@boost.co.nz>
2019-10-18 10:31:40 +1300

    Updated the migrations to use slack_workspace instead of team and changed the code review, reviewer/developer relationship to be a 1-many

--- db/migrate/20191004012007_create_code_reviews.rb
+++ db/migrate/20191004012007_create_code_reviews.rb
@@ -4,9 +4,7 @@ class CreateCodeReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :code_reviews do |t|
       t.string :url
-      t.belongs_to :team
-      t.references :reviewer1, foreign_key: { to_table: 'developers' }
-      t.references :reviewer2, foreign_key: { to_table: 'developers' }
+      t.belongs_to :slack_workspace
 
       t.timestamps
     end

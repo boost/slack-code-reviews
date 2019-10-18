315e2467176161f070866e3b86e583ad58981006
Montgomery Anderson <montgomery@boost.co.nz>
2019-10-18 13:36:57 +1300

    Added spaces to list developers and projects

--- app/services/slack/action/list_developers.rb
+++ app/services/slack/action/list_developers.rb
@@ -5,7 +5,11 @@ module Slack
     class ListDevelopers < Slack::AbstractAction
       def initialize(slack_workspace)
         super(slack_workspace)
-        developers = Developer.where(slack_workspace: @slack_workspace).map { |d| "    - <#{d.name}>" }
+
+        developers = Developer.where(slack_workspace: @slack_workspace).map do |developer|
+          "    - <#{developer.name}>"
+        end
+
         @text = (['Developer list:'] + developers).join("\n")
         @text = 'No developers added yet.' if developers.empty?
       end
--- app/services/slack/action/list_projects.rb
+++ app/services/slack/action/list_projects.rb
@@ -5,7 +5,11 @@ module Slack
     class ListProjects < Slack::AbstractAction
       def initialize(slack_workspace)
         super(slack_workspace)
-        projects = Project.where(slack_workspace: @slack_workspace).map { |d| "    - <#{d.name}>" }
+
+        projects = Project.where(slack_workspace: @slack_workspace).map do |developer|
+          "    - <#{developer.name}>"
+        end
+
         @text = (['Projects list:'] + projects).join("\n")
         @text = 'No projects added yet.' if projects.empty?
       end

18d203b86311bf45e0c8580cf494c38b52dd3b4e
Montgomery Anderson <montgomery@boost.co.nz>
2019-10-18 13:34:36 +1300

    Implemented simple list projects slack action

---
+++ app/services/slack/action/list_projects.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Slack
+  module Action
+    class ListProjects < Slack::AbstractAction
+      def initialize(slack_workspace)
+        super(slack_workspace)
+        projects = Project.where(slack_workspace: @slack_workspace).map { |d| "    - <#{d.name}>" }
+        @text = (['Projects list:'] + projects).join("\n")
+        @text = 'No projects added yet.' if projects.empty?
+      end
+    end
+  end
+end

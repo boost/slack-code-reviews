042ca4adc107e0ec424889e9fa246abbe62dcb81
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2019-12-01 18:16:29 +1300

    simplify the pick up system with a SQL queue query

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -11,33 +11,11 @@ class Developer < ApplicationRecord
   scope :havnt_recieved_a_review, -> { where(code_reviews: []) }
 
   class << self
-    def pick_reviewer(workspace, name: nil, exclude: [], project: nil)
-      return pick_specific_reviewer(workspace, name) if name
+    def queue
+      developer_queue = DevelopersCodeReview.developer_queue.to_sql
 
-      pick_next_reviewer(workspace, exclude: exclude, project: project)
-    end
-
-    # Pick a specific develoepr for code review
-    def pick_specific_reviewer(workspace, name)
-      reviewer = find_by(slack_workspace: workspace, name: name)
-      return reviewer if reviewer.present?
-
-      raise ActiveRecord::RecordNotFound, "<#{name}> not found"
-    end
-
-    # Pick the next developer up for code review
-    def pick_next_reviewer(workspace, exclude: [], project: nil)
-      workspace_project_developers = in_slack_workspace(workspace).where(project: project)
-
-      available_developers = workspace_project_developers.to_a - exclude
-
-      reviewer = available_developers.min_by do |developer|
-        developer.code_reviews.last&.created_at || DateTime.new(0) # along time ago
-      end
-
-      return reviewer if reviewer.present?
-
-      raise ActiveRecord::RecordNotFound, 'Could not find an available reviewer'
+      joins("LEFT OUTER JOIN (#{developer_queue}) dcr ON id = dcr.developer_id").
+      order(max_updated_at: :asc)
     end
   end
 end
---
+++ app/services/slack/action/add_code_review.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+module Slack
+  module Action
+    class AddCodeReview < Slack::AbstractAction
+      REQUIRED_NUMBER_OF_REVIEWERS = 2
+      PREFERED_NUMBER_OF_REVIEWERS_IN_PROJECT = 1
+
+      def initialize(slack_workspace, url, requester, given_reviewers_names)
+        super(slack_workspace)
+
+        reviewers = pick_reviewers_by_name(given_reviewers_names)
+
+        reviewers += pick_reviewers_in_project(reviewers, requester)
+        reviewers += pick_external_reviewers(reviewers, requester)
+
+        create_code_review(url, reviewers)
+      rescue ActiveRecord::RecordNotFound => e
+        @visibility = :ephemeral
+        @text = e.message
+      end
+
+    private
+      def create_code_review(url, reviewers)
+        CodeReview.create(slack_workspace: @slack_workspace, url: url, developers: reviewers)
+
+        @visibility = :in_channel
+        @text = "New CR #{url} " + reviewers.map(&:name).join(' ')
+      end
+
+      def pick_reviewers_in_project(reviewers, requester)
+        return [] if requester.project.nil?
+
+        to_take = PREFERED_NUMBER_OF_REVIEWERS_IN_PROJECT -
+                  reviewers.count { |reviewer| reviewer.project == requester.project }
+
+        # developers part of the project but not given by the user
+        Developer.
+          queue.
+          where.not(id: reviewers + [requester]).
+          where(project: requester.project).
+          where(slack_workspace: @slack_workspace).
+          limit(to_take)
+      end
+
+      def pick_external_reviewers(reviewers, requester)
+        to_take = REQUIRED_NUMBER_OF_REVIEWERS - reviewers.length
+
+        Developer.
+          queue
+          .where.not(id: reviewers + [requester]).
+          where(slack_workspace: @slack_workspace).
+          limit(to_take)
+      end
+
+      def pick_reviewers_by_name(names)
+        return [] if names.empty?
+
+        names.map { |name| Developer.find_by(slack_workspace: @slack_workspace, name: name) }
+      end
+    end
+  end
+end

beec8acec6210ed64bb7ce8c29ecfcfeff0db8b8
Dave Harris <dave@boost.co.nz>
2020-08-07 10:59:52 +1200

    feature(security): Use Github App to
    * Created private key via Github admin
    * Generate JWT
    * Generate Access Token with JWT

--- app/services/github/api/request.rb
+++ app/services/github/api/request.rb
@@ -1,23 +1,20 @@
 # frozen_string_literal: true
 
-module Github
-  module Api
-    # Mother class for api calls
-    class Request
-      BASE_URL = 'https://api.github.com'
+# Mother class for api calls
+class Github::Api::Request
+  BASE_URL = 'https://api.github.com'
 
-      def self.post(path:, payload:)
-        response = RestClient.post(
-          "#{BASE_URL}#{path}",
-          payload.to_json,
-          content_type: :json, accept: :json, charset: 'utf-8',
-          authorization: "token #{ENV['GITHUB_PERSONAL_ACCESS_TOKEN']}"
-        )
-        JSON.parse(response)
-      rescue JSON::ParserError => e
-        Rails.logger.debug(e)
-        response
-      end
-    end
+  def self.post(path:, payload:)
+    response = RestClient.post(
+      "#{BASE_URL}#{path}",
+      payload.to_json,
+      authorization: "Token #{AccessToken.get}"
+    )
+
+    JSON.parse(response)
+  rescue JSON::ParserError => e
+    Rails.logger.debug(e)
+    response
   end
+
 end
--- app/services/github/api/request_reviewers.rb
+++ app/services/github/api/request_reviewers.rb
@@ -1,26 +1,24 @@
 # frozen_string_literal: true
 
-module Github
-  module Api
-    class RequestReviewers < Request
-      def self.post(url, developers)
-        github_names = developers.map(&:github_name).keep_if(&:present?)
-        return if github_names.empty?
+class Github::Api::RequestReviewers < Github::Api::Request
 
-        path = [
-          '/repos',
-          url.organisation,
-          url.project,
-          'pulls',
-          url.request_id,
-          'requested_reviewers'
-        ].join('/')
+  def self.post(code_review_url, developers)
+    github_names = developers.map(&:github_name).keep_if(&:present?)
+    return if github_names.empty?
 
-        super(
-          path: path,
-          payload: { reviewers: github_names }
-        )
-      end
-    end
+    path = [
+      '/repos',
+      code_review_url.organisation,
+      code_review_url.project,
+      'pulls',
+      code_review_url.request_id,
+      'requested_reviewers'
+    ].join('/')
+
+    super(
+      path: path,
+      payload: { reviewers: github_names }
+    )
   end
+
 end

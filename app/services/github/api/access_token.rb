ab2fdd92e4ea4bd1ebfa512f90d96858acae6e0f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-08-07 12:24:29 +1200

    fix(github sending): fixes the code for github sending

--- app/services/github/api/access_token.rb
+++ app/services/github/api/access_token.rb
@@ -1,42 +1,42 @@
 # frozen_string_literal: true
 
-class Github::Api::AccessToken < Github::Api::Request
+module Github
+  module Api
+    class AccessToken < Github::Api::Request
+      def self.get
+        response = RestClient.post(
+          "#{BASE_URL}/app/installations/#{ENV['GITHUB_APP_INSTALLATION_ID']}/access_tokens",
+          {}.to_json,
+          accept: 'application/vnd.github.machine-man-preview+json',
+          authorization: "Bearer #{generate_jwt}"
+        )
 
-  def self.get
-    response = RestClient.post(
-      "#{BASE_URL}/app/installations/#{ENV['GITHUB_APP_INSTALLATION_ID']}/access_tokens",
-      {}.to_json,
-      accept: 'application/vnd.github.machine-man-preview+json',
-      authorization: "Bearer #{generate_jwt}"
-    )
+        # {
+        #   "token"=>"v1.2f...",
+        #   "expires_at"=>"2020-06-29T21:52:53Z",
+        #   "permissions"=>{"metadata"=>"read", "pull_requests"=>"write"},
+        #   "repository_selection"=>"all"
+        # }
 
-    # {
-    #   "token"=>"v1.2f...",
-    #   "expires_at"=>"2020-06-29T21:52:53Z",
-    #   "permissions"=>{"metadata"=>"read", "pull_requests"=>"write"},
-    #   "repository_selection"=>"all"
-    # }
+        JSON.parse(response)['token']
+      rescue JSON::ParserError => e
+        Rails.logger.debug(e)
+        response
+      end
 
-    JSON.parse(response)['token']
-  rescue JSON::ParserError => e
-    Rails.logger.debug(e)
-    response
-  end
+      def self.generate_jwt
+        payload = {
+          iat: Time.current.to_i,        # Issued at
+          exp: 10.minutes.from_now.to_i, # Expires at (10 minute maximum)
+          iss: ENV['GITHUB_APP_ID']
+        }
 
-  private_key
+        private_key = OpenSSL::PKey::RSA.new(
+          ENV['GITHUB_APP_PRIVATE_KEY']
+        )
 
-    def generate_jwt
-      payload = {
-        iat: Time.current.to_i,        # Issued at
-        exp: 10.minutes.from_now.to_i, # Expires at (10 minute maximum)
-        iss: ENV['GITHUB_APP_ID']
-      }
-
-      private_key = OpenSSL::PKey::RSA.new(
-        Base64.decode64(ENV['GITHUB_APP_PRIVATE_KEY'])
-      )
-
-      JWT.encode(payload, private_key, 'RS256')
+        JWT.encode(payload, private_key, 'RS256')
+      end
     end
-
+  end
 end

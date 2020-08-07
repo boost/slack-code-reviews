0c165b7a848b8fb0d6c361fb89f7bf1ad1d977eb
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2020-08-07 12:24:29 +1200

    fix(github sending): fixes the code for github sending

--- app/services/github/api/request.rb
+++ app/services/github/api/request.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
 # Mother class for api calls
-class Github::Api::Request
-  BASE_URL = 'https://api.github.com'
+module Github
+  module Api
+    class Request
+      BASE_URL = 'https://api.github.com'
 
-  def self.post(path:, payload:)
-    response = RestClient.post(
-      "#{BASE_URL}#{path}",
-      payload.to_json,
-      authorization: "Token #{AccessToken.get}"
-    )
+      def self.post(path:, payload:)
+        token = Github::Api::AccessToken.get
+        # binding.pry
+        response = RestClient.post(
+          "#{BASE_URL}#{path}",
+          payload.to_json,
+          authorization: "Token #{token}"
+        )
 
-    JSON.parse(response)
-  rescue JSON::ParserError => e
-    Rails.logger.debug(e)
-    response
+        JSON.parse(response)
+      rescue JSON::ParserError => e
+        Rails.logger.debug(e)
+        response
+      rescue RestClient::UnprocessableEntity => e
+        Rails.logger.debug(e)
+      end
+    end
   end
-
 end
--- app/services/github/api/request_reviewers.rb
+++ app/services/github/api/request_reviewers.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-class Github::Api::RequestReviewers < Github::Api::Request
+module Github
+  module Api
+    class RequestReviewers < Github::Api::Request
+      def self.post(code_review_url, developers)
+        github_names = developers.map(&:github_name).keep_if(&:present?)
+        return if github_names.empty?
 
-  def self.post(code_review_url, developers)
-    github_names = developers.map(&:github_name).keep_if(&:present?)
-    return if github_names.empty?
+        path = [
+          '/repos',
+          code_review_url.organisation,
+          code_review_url.project,
+          'pulls',
+          code_review_url.request_id,
+          'requested_reviewers'
+        ].join('/')
 
-    path = [
-      '/repos',
-      code_review_url.organisation,
-      code_review_url.project,
-      'pulls',
-      code_review_url.request_id,
-      'requested_reviewers'
-    ].join('/')
-
-    super(
-      path: path,
-      payload: { reviewers: github_names }
-    )
+        super(
+          path: path,
+          payload: { reviewers: github_names }
+        )
+      end
+    end
   end
-
 end
